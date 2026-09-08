af5c2ef808ec79c90697d7b786854a541cd02f72
17olsp17-cell <17ol.sp17@gmail.com>
2026-09-08 18:03:14 -0400

    Create hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,18 @@
+//Olena
+// Assigment 1
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main () {
+
+cout << "Hello, word!" << endL;
+cout << "What is your name?";
+string name;
+cin >> name;
+
+count << "Nice to meet you, " << Olena << "!" << endL;
+
+return 0;
