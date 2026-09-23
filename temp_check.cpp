6a2b82503be80fa5ff627a0ad216eaed77856612
17olsp17-cell <17ol.sp17@gmail.com>
2026-09-23 18:50:58 -0400

    temp_check.cpp

---
+++ temp_check.cpp
@@ -0,0 +1,24 @@
+#include <iostream> //input,output library
+using namespace std; //standard namespace
+
+int main() //main function
+{
+    int temperature; //temperature variable 
+    
+    cout << "Enter the temperature (F):"; //ask temperature
+    cin >> temperature; //get temperature
+    
+    if (temperature < 32) { //check freezing 
+        cout << "That's Freezing."; //print freezing 
+    }
+    else if (temperature <= 59) {  //check cold 
+        cout << "That's Cold.";  //print cold 
+    }
+    else if (temperature <=75) {  //check mild
+        cout << "That's Mild.";  //print mild
+    }
+    else {  // otherwise
+        cout << "That's Hot.";  //print hot 
+    }
+     return 0;  //end program  
+}
